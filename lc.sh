117ca216fe1b76ee13639da16a3c45a00f6bfa84
leoye <leoyey@hotmail.com>
2017-02-11 19:19:18 +0800

    remove .jks file before convertion

--- lc.sh
+++ lc.sh
@@ -4,14 +4,16 @@ FULLCHAIN=$2
 PRIVKEY=$3
 STOREPASS=$4
 OUTDIR=$5
+KEYTOOL=/usr/local/jdk/bin/keytool
 
 if [ "$#" -ne 5 ];
 then
-	echo `basename "$0"` domain.tld fullchain.pem privkey.pem password outdir
+        echo `basename "$0"` domain.tld fullchain.pem privkey.pem password outdir
 else
-	keytool -import -alias $DOMAIN -keystore $DOMAIN.jks -file $FULLCHAIN -storepass $STOREPASS -noprompt
-	openssl pkcs12 -export -in $FULLCHAIN -inkey $PRIVKEY -password pass:$STOREPASS > $DOMAIN-server.p12
-	keytool -importkeystore -srckeystore $DOMAIN-server.p12 -destkeystore $DOMAIN.jks -srcstoretype pkcs12 -storepass $STOREPASS -srcstorepass $STOREPASS
-	rm $DOMAIN-server.p12
-	mv $DOMAIN.jks $OUTDIR
+        rm -f $OUTDIR/$DOMAIN.jks
+        $KEYTOOL -import -alias $DOMAIN -keystore $OUTDIR/$DOMAIN.jks -file $FULLCHAIN -storepass $STOREPASS -noprompt
+        openssl pkcs12 -export -in $FULLCHAIN -inkey $PRIVKEY -password pass:$STOREPASS > $OUTDIR/$DOMAIN-server.p12
+        $KEYTOOL -importkeystore -srckeystore $OUTDIR/$DOMAIN-server.p12 -destkeystore $OUTDIR/$DOMAIN.jks -srcstoretype pkcs12 -storepass $STOREPASS -srcstorepass $STOREPASS
+        $KEYTOOL -list -keystore $OUTDIR/$DOMAIN.jks -storepass $STOREPASS
+        rm $OUTDIR/$DOMAIN-server.p12
 fi
